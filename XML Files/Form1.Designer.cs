ad5f2ec483e8bab6d2e33ffffea81c40c68ec6d7
Gabriel Torres <elcomparosh97@gmail.com>
2025-02-22 16:56:44 -0600

    .

--- XML Files/Form1.Designer.cs
+++ XML Files/Form1.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtSearch = new TextBox();
+            btnLoad = new Button();
+            btnSave = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(22, 39);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(265, 23);
+            txtSearch.TabIndex = 0;
+            txtSearch.TextChanged += txtSearch_TextChanged;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(22, 78);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(112, 30);
+            btnLoad.TabIndex = 1;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(161, 78);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(112, 30);
+            btnSave.TabIndex = 2;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 125);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(776, 313);
+            dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnSave);
+            Controls.Add(btnLoad);
+            Controls.Add(txtSearch);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtSearch;
+        private Button btnLoad;
+        private Button btnSave;
+        private DataGridView dataGridView1;
     }
 }
--- XML Files/Form1.cs
+++ XML Files/Form1.cs
@@ -1,10 +1,90 @@
+using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
 namespace XML_Files
 {
     public partial class Form1 : Form
     {
+        private DataSet dataSet;
+
         public Form1()
         {
             InitializeComponent();
+            dataSet = new DataSet();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XML Files|*.xml";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    dataSet.ReadXml(openFileDialog.FileName);
+                    dataGridView1.DataSource = dataSet.Tables[0];
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading the XML file: " + ex.Message);
+
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "XML Files|*.xml";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    dataSet.WriteXml(saveFileDialog.FileName);
+                    MessageBox.Show("File saved successfully.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error saving the XML file: " + ex.Message);
+                }
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string valorBuscar = txtSearch.Text.Trim();
+
+            if (dataSet.Tables.Count > 0)
+            {
+                if (string.IsNullOrEmpty(valorBuscar))
+                {
+                    dataGridView1.DataSource = dataSet.Tables[0];
+                }
+                else
+                {
+                    var resultados = dataSet.Tables[0].AsEnumerable()
+                        .Where(row => row.ItemArray.Any(field => field.ToString().Contains(valorBuscar)))
+                        .ToList();
+
+                    if (resultados.Any())
+                    {
+                        DataTable dtResultados = resultados.CopyToDataTable();
+                        dataGridView1.DataSource = dtResultados;
+                    }
+                    else
+                    {
+                        dataGridView1.DataSource = null;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please load a file first.");
+            }
         }
     }
 }
